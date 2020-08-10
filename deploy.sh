794878ec1af09dd995e32773121cde198176f940
Chen Lin Chu <chenlinchu@ChendeMac-mini.local>
2020-08-10 21:10:31 +0800

    2020.08.10_updated

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+echo "------------------------------------------------------"
+echo "-------------------無腦push專案開始-------------------"
+echo "------------------------------------------------------"
+
+dateToday="$(date +'%Y.%m.%d')"
+
+set -e
+
+npm run build
+
+git add .
+
+git commit -m "${dateToday}_updated"
+
+git push -u origin master
+
+npm run dev
